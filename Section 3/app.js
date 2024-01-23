348659d78e724e1ffb1c89be49c3a3c69e4454c4
Deric <drccsadrs@gmail.com>
2024-01-24 00:11:22 +0800

    sending responses

--- Section 3/app.js
+++ Section 3/app.js
@@ -9,6 +9,12 @@ const server = http.createServer((req, res) => {
     // important values req.url, req.metod, req headers
     console.log(req.url, req.method, req.headers);
     //process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>')
+    res.write('<head><title>My first Page</title><head>')
+    res.write('<body><h1>Hello from my Node.js Server! <h1> </body>');
+    res.write('<html>')
+    res.end()
 });
 
 server.listen(3000);
