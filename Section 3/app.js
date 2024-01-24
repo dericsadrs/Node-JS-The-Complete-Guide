c41125c35598ac2f53ebdeff813024b06b854104
Deric <drccsadrs@gmail.com>
2024-01-24 23:21:21 +0800

    //add comments

--- Section 3/app.js
+++ Section 3/app.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
+
+  // this will load when you browse the localhost
   if (url === '/') {
     res.write('<html>');
     res.write('<head><title>Enter Message</title><head>');
@@ -11,15 +13,19 @@ const server = http.createServer((req, res) => {
     res.write('</html>');
     return res.end();
   }
+  
   if (url === '/message' && method === 'POST') {
     const body = [];
+
+    //event listener buffer
     req.on('data', (chunk) => {
       console.log(chunk);
       body.push(chunk);
     });
+
     req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
+      const message = parsedBody.split('=')[1]; 
       fs.writeFileSync('message.txt', message);
     });
     res.statusCode = 302;
