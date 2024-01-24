b51191633770a8235950228d54c4ec346eeab748
Deric <drccsadrs@gmail.com>
2024-01-25 00:38:33 +0800

    assignment 1 solution

---
+++ Section 3/assignment1/assignment1Route.js
@@ -0,0 +1,46 @@
+
+
+const requestHandler = (req,res) => {
+
+    //get the url for checking for endpoints
+    const url = req.url;
+
+    // default slash 
+    if (url === '/') {
+      res.setHeader('Content-Type', 'text/html');
+      res.write('<html>');
+      res.write('<head><title>Assignment 1</title></head>');
+      res.write(
+        '<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>'
+      );
+      res.write('</html>');
+      return res.end();
+    }
+    // route for /users
+    if (url === '/users') {
+      res.setHeader('Content-Type', 'text/html');
+      res.write('<html>');
+      res.write('<head><title>Assignment 1</title></head>');
+      res.write('<body><ul><li>User 1</li><li>User 2</li></ul></body>');
+      res.write('</html>');
+      return res.end();
+    }
+    // Send a HTML response with some "Page not found text
+    if (url === '/create-user') {
+      const body = [];
+      req.on('data', chunk => {
+        body.push(chunk);
+      });
+      req.on('end', () => {
+        const parsedBody = Buffer.concat(body).toString();
+        console.log(parsedBody.split('=')[1]); // username=whatever-the-user-entered
+      });
+      res.statusCode = 302;
+      res.setHeader('Location', '/');
+      res.end();
+    }
+}
+
+module.exports = {
+    handler: requestHandler,
+}
